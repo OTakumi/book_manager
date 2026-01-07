a9b3016ebb6d1967bf30aac6d38cbf2b46389bb5
OTakumi <takumi.o.0410@gmail.com>
2026-01-07 17:16:24 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::net::{Ipv4Addr, SocketAddr};
+
+use anyhow::Result;
+use axum::{Router, http::StatusCode, routing::get};
+use tokio::net::TcpListener;
+
+pub fn main() {
+    println!("Hello, world!");
+}
