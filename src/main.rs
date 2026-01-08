927fba0546244ffa2df6b1f53b94454b9585cf40
OTakumi <takumi.o.0410@gmail.com>
2026-01-08 16:24:37 +0900

    feat: データベース接続設定とプール生成の追加、環境変数から設定を取得
    - DatabaseConfig構造体を追加し、環境変数からDB設定を読み込む処理を実装
    - PgConnectOptionsへの変換と接続プール生成関数を実装
    - アプリケーションの状態としてDBコネクションプールをRouterに追加

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::net::{Ipv4Addr, SocketAddr};
-
+use anyhow::{Context, Result};
 use axum::{Router, http::StatusCode, routing::get};
+use sqlx::Database;
+use sqlx::{PgPool, postgres::PgConnectOptions};
+use std::env;
+use std::net::{Ipv4Addr, SocketAddr};
 use tokio::net::TcpListener;
 
 /// Health check handler
@@ -13,12 +16,67 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+struct DatabaseConfig {
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub database: String,
+}
+
+impl DatabaseConfig {
+    /// Read environment variables from a .env file
+    fn from_env() -> Result<Self> {
+        if let Err(e) = dotenvy::dotenv()
+            && !e.not_found()
+        {
+            return Err(e).context("Faild to load .env file");
+        }
+
+        let db_port = env::var("POSTGRES_PORT")
+            .expect("DB port not set")
+            .to_string();
+        let db_username = env::var("POSTGRES_USER").expect("DB user name not set");
+        let db_password = env::var("POSTGRES_PASSWORD").expect("DB password not set");
+        let db_name = env::var("POSTGRES_DB").expect("DB name not set");
+
+        Ok(DatabaseConfig {
+            host: "localhost".into(),
+            port: db_port.parse::<u16>().unwrap(),
+            username: db_username,
+            password: db_password,
+            database: db_name,
+        })
+    }
+}
+
+/// Create a struct for Postgres from DatabaseConfig
+impl From<DatabaseConfig> for PgConnectOptions {
+    fn from(cfg: DatabaseConfig) -> Self {
+        Self::new()
+            .host(&cfg.host)
+            .port(cfg.port)
+            .username(&cfg.username)
+            .password(&cfg.password)
+            .database(&cfg.database)
+    }
+}
+
+/// Generate a database connection pool
+fn connect_database_with(cfg: DatabaseConfig) -> PgPool {
+    PgPool::connect_lazy_with(cfg.into())
+}
+
 #[tokio::main]
 async fn main() {
+    let database_config = DatabaseConfig::from_env().unwrap();
+    let conn_pool = connect_database_with(database_config);
+
     // Router config
     let app = Router::new()
         .route("/hello", get(hello_world))
-        .route("/health", get(health_check));
+        .route("/health", get(health_check))
+        .with_state(conn_pool);
 
     // Listen for requests on localhost:8080
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
